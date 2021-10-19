e11451e37cc9c428b83b796093e66bf540b53f7c
墨七 <meichangliang@mo7.cc>
2021-10-19 10:06:13 +0800

    add

---
+++ basic/基本数据类型/dictionary.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# 字典
+
+
+'''
+
+dict = {}
+dict['one'] = "1 - 菜鸟教程"
+dict[2] = "2 - 菜鸟工具"
+
+tinydict = {'name': 'runoob', 'code': 1, 'site': 'www.runoob.com'}
+
+
+print(dict['one'])       # 输出键为 'one' 的值
+print(dict[2])           # 输出键为 2 的值
+print(tinydict)          # 输出完整的字典
+print(tinydict.keys())   # 输出所有键
+print(tinydict.values())  # 输出所有值
+'''
+
+
+# 运算符 ， x 的 y 次幂
+
+# a = 0.3
+# b = 20
+
+# print(a**b)
+
+
+a = 0
+b = 20
+
+print(a and b)
