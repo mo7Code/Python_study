ae1887f77852f856d0157e74d610c97dbab75efc
墨七 <meichangliang@mo7.cc>
2021-10-21 16:36:18 +0800

    add

--- basic/基本数据类型/string.py
+++ basic/基本数据类型/string.py
@@ -8,4 +8,23 @@ var1 = 'Hello World!'
 print("已更新字符串 : ", var1[:6] + 'Runoob!')
 '''
 
+'''
 print("\a")
+'''
+
+'''
+str1 = '''
+阿斯达
+阿斯达
+阿斯达
+啊啊啊
+'''
+
+print(str1)
+'''
+
+name = "roboot"
+
+f'hello {name}'
+
+print(name)
