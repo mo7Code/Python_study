e9c1f4247965b9ec54d551251a7433883ff4acb3
墨七 <meichangliang@mo7.cc>
2021-10-24 16:26:10 +0800

    add

---
+++ basic/基本数据类型/dict.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+# 字典
+
+# 键必须是唯一的，但值则不必。
+
+# 值可以取任何数据类型，但键必须是不可变的，如字符串，数字。
+
+
+dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+
+# print("dict['Alice']: ", dict['Alice'])
+print("dict['Alice']: ", dict['Name'])
