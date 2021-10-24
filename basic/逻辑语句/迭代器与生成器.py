e85702be57083f37d10f5cef9b163d31ba129d1e
墨七 <meichangliang@mo7.cc>
2021-10-24 18:49:27 +0800

    add

---
+++ basic/逻辑语句/迭代器与生成器.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+# 迭代器与生成器
+list = [1, 2, 3, 4]
+it = iter(list)
+
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
