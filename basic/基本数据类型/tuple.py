5dfe27c81ca4ce54fe10e4eb9f2668825a639b73
墨七 <meichangliang@mo7.cc>
2021-10-24 16:17:44 +0800

    Add

--- basic/基本数据类型/tuple.py
+++ basic/基本数据类型/tuple.py
@@ -12,3 +12,8 @@ print(tuple[1:3])        # 输出从第二个元素开始到第三个元素
 print(tuple[2:])         # 输出从第三个元素开始的所有元素
 print(tinytuple * 2)     # 输出两次元组
 print(tuple + tinytuple)  # 连接元组
+
+
+# 的元组与列表类似，不同之处在于元组的元素不能修改。
+# 元祖是一个只读类型的 list
+# 数组
