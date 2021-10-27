003b60a69f8ff910d58856c90fcb69b69651212d
墨七 <meichangliang@mo7.cc>
2021-10-27 10:20:01 +0800

    add

--- basic/逻辑语句/func.py
+++ basic/逻辑语句/func.py
@@ -1,3 +1,37 @@
 #!/usr/bin/python3
 
 # 函数
+
+# def hello():
+#     print("hello")
+
+
+# hello()
+
+# a = [123]
+# a = "123"
+
+# print(a)
+
+
+# def change(a):
+#     print(id(a))   # 指向的是同一个对象
+#     a = 10
+#     print(id(a))   # 一个新对象
+
+
+# a = 1
+# print(id(a))
+# change(a)
+
+v = 1
+
+
+def change(v):
+    v = 123
+    print("func-out", id(v))
+
+
+change(v)
+
+print("out", id(v))
--- sync.sh
+++ sync.sh
@@ -3,6 +3,6 @@
 # find . -name "*.exe" | xargs rm -rf
 # find . -name "*.pdb" | xargs rm -rf
 git pull &&
-git add . &&
-git commit -m 'add' &&
-git push
+  git add . &&
+  git commit -m 'add' &&
+  git push
